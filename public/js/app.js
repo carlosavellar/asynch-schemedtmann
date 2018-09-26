3493ea0f865f2c88e62c94085d3f3bbbf64d6bab
José Carlos <carlos.avellar@gmail.com>
2018-09-26 17:30:15 -0300

    First promisse(), then

--- public/js/app.js
+++ public/js/app.js
@@ -20,18 +20,29 @@ thirth();
 
 // Simulating Ajax
 
-const getRecipe = ()=>{
-    setTimeout(() => {
-        const menu = [223,455,787,333];
-        console.log(menu);
-        setTimeout(id=>{
-            const recipe = { title: "Tomato estrogonofrato", publisher: 'José Carlos' };
-            console.log(`${id} e o sei lá ${recipe.publisher}`);
-            setTimeout(publisher => {
-                const recipe2 = { title: "Italian", publisher: "José" };
-                console.log(recipe);
-            }, 1500, recipe.publisher);
-        }, 1500, menu[2]);
-    }, 1500);
-};
-getRecipe();
+// const getRecipe = ()=>{
+//     setTimeout(() => {
+//         const menu = [223,455,787,333];
+//         console.log(menu);
+//         setTimeout(id=>{
+//             const recipe = { title: "Tomato estrogonofrato", publisher: 'José Carlos' };
+//             console.log(`${id} e o sei lá ${recipe.publisher}`);
+//             setTimeout(publisher => {
+//                 const recipe2 = { title: "Italian", publisher: "José" };
+//                 console.log(recipe);
+//             }, 1500, recipe.publisher);
+//         }, 1500, menu[2]);
+//     }, 1500);
+// };
+// getRecipe();
+
+
+const getID = new Promise((resolve, reject)=>{
+    setTimeout(()=>
+        resolve([[223,455,787,333]]),
+    1500);
+});
+
+getID.then(IDs=>{
+    console.log(IDs);
+});
