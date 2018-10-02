89cbbaefcb2c0d41e5f950279d0809e0a0e43713
José Carlos <carlos.avellar@gmail.com>
2018-10-02 09:41:56 -0300

    Promisse ok

--- public/js/app.js
+++ public/js/app.js
@@ -47,35 +47,50 @@ thirth();
 //     console.log(IDs);
 // });
 
-const dirIDs = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve([223,455,787,333]);
+const dirIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([223, 455, 787, 333]);
     }, 1500);
 });
 
-const geTRecipe =  recID => {
-    return new Promisse((resolve, reject)=>{
+const geTRecipe = recID => {
+    return new Promisse((resolve, reject) => {
         setTimeout(ID, 1500, recID);
     });
 };
 
-const getrecipe2 = RecID =>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(ID=>{
-            const recipe = { title: "Tomato estrogonofrato", publisher: 'José Carlos' };
-            console.log(`${ID} e o sei lá ${recipe.publisher}`);
+const getrecipe2 = RecID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: "Tomato estrogonofrato",
+                publisher: 'José Carlos'
+            };
+            resolve(`${ID} e o sei lá ${recipe.publisher}`);
         }, 1500, RecID);
     });
 };
+const getRelated = rela =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(someRec =>{
+            const recipe2 = { title: "Italian", publisher: "José" };
+            resolve(`${recipe2.title} and puplidher is ${recipe2.publisher}`);
+        }, 1500, rela);
+    });
+};
 dirIDs
-  .then(IDs=>{
-    console.log(IDs);
-    return getrecipe2(IDs[2]);
-})
-   .then(()=>{
-       console.log(recipe);
-   })
-.catch(err=>{
-    console.log(err);
-});
+    .then(IDs => {
+        console.log(IDs);
+        return getrecipe2(IDs[2]);
+    })
+    .then(recipe=>{
+        console.log(recipe);
+        return getRelated(recipe);
+    })
+    .then(recipe => {
+        console.log(recipe);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
