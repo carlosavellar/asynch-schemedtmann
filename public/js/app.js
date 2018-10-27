bb6c77dbd671453377d34d1f45d8e9e1f6304add
José Carlos <carlos.avellar@gmail.com>
2018-10-27 15:54:40 -0300

    Promises Again

--- public/js/app.js
+++ public/js/app.js
@@ -47,37 +47,37 @@ thirth();
 //     console.log(IDs);
 // });
 
-const dirIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([223, 455, 787, 333]);
-    }, 1500);
-});
+// const dirIDs = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve([223, 455, 787, 333]);
+//     }, 1500);
+// });
 
-const geTRecipe = recID => {
-    return new Promisse((resolve, reject) => {
-        setTimeout(ID, 1500, recID);
-    });
-};
+// const geTRecipe = recID => {
+//     return new Promisse((resolve, reject) => {
+//         setTimeout(ID, 1500, recID);
+//     });
+// };
 
-const getrecipe2 = RecID => {
-    return new Promise((resolve, reject) => {
-        setTimeout(ID => {
-            const recipe = {
-                title: "Tomato estrogonofrato",
-                publisher: 'José Carlos'
-            };
-            resolve(`${ID} e o sei lá ${recipe.publisher}`);
-        }, 1500, RecID);
-    });
-};
-const getRelated = rela =>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(someRec =>{
-            const recipe2 = { title: "Italian", publisher: "José" };
-            resolve(`${recipe2.title} and puplidher is ${recipe2.publisher}`);
-        }, 1500, rela);
-    });
-};
+// const getrecipe2 = RecID => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(ID => {
+//             const recipe = {
+//                 title: "Tomato estrogonofrato",
+//                 publisher: 'José Carlos'
+//             };
+//             resolve(`${ID} e o sei lá ${recipe.publisher}`);
+//         }, 1500, RecID);
+//     });
+// };
+// const getRelated = rela =>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(someRec =>{
+//             const recipe2 = { title: "Italian", publisher: "José" };
+//             resolve(`${recipe2.title} and puplidher is ${recipe2.publisher}`);
+//         }, 1500, rela);
+//     });
+// };
 // dirIDs
 //     .then(IDs => {
 //         console.log(IDs);
@@ -96,19 +96,50 @@ const getRelated = rela =>{
 
 // Asynch Await
 
-async function getrecipeAW (){
-    const IDs = await(dirIDs);
-    console.log(IDs);
-    const recipe = await(getrecipe2(IDs[2]));
-    console.log(recipe);
-    const related = await(getRelated(recipe));
-    console.log(related);
-    return recipe;
-}
+// async function getrecipeAW (){
+//     const IDs = await(dirIDs);
+//     console.log(IDs);
+//     const recipe = await(getrecipe2(IDs[2]));
+//     console.log(recipe);
+//     const related = await(getRelated(recipe));
+//     console.log(related);
+//     return recipe;
+// }
 
 // This aboive is synchronous
 // const rec = getrecipeAW();
 // console.log(rec);
-getrecipeAW().then(result=>{
-    console.log(`This is the best: ${result}`);
-});
+// getrecipeAW().then(result=>{
+//     console.log(`This is the best: ${result}`);
+// });
+
+// consuming data from a real API
+
+// fetch('https://www.crossorigin.me/https://www.metaweather.com/api/location/2487956');
+
+
+
+//Old Asynchronous code
+
+// const sayRecipie = ()=>{
+//     setTimeout(() => {
+//         const recipiesID =  [123,445,899,445];
+//         console.log(recipiesID);
+//             setTimeout((id) => {
+//                 const recipe = { publisher: 'Carlos', title: 'Potarto Craralho', cor: 'azul' };
+//                 console.log(`${id}: ${recipe.publisher}`);
+//                 setTimeout((publisher) => {
+//                     const recipe2 = { publisher: 'Mastodont', title: 'Carilho', cor: 'azul' };
+//                     console.log(recipe2);
+//                 }, 2000, recipe.publisher);
+//             }, 1000, recipiesID[2]);
+
+//     }, 1500);
+// };
+
+
+// sayRecipie();
+
+
+//Promises
+
